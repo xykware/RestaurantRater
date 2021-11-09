611b2db8534c1e191f3c755bcc4af8ebeb8ffb0e
XykWare <ethan.taylor.richardson@gmail.com>
2021-11-09 11:50:43 -0500

    added Details view

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -101,5 +101,23 @@ namespace RestaurantRater.Controllers
 
             return View(restaurant);
         }
+
+        // GET: Restaurant/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = _db.Restaurants.Find(id);
+
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(restaurant);
+        }
     }
 }
